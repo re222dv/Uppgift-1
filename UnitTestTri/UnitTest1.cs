ecbc3a629fb33e55cd2e1f834a31cd5734f48ae5
re222dv <re222dv@student.lnu.se>
2013-11-12 14:47:42 +0100

    Add some unit tests

---
+++ UnitTestTri/UnitTest1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestTri {
+    [TestClass]
+    public class TriangleUnitTest {
+        [TestMethod]
+        public void isScaleneTest() {
+            Triangle tri = new Triangle(1.0, 0.5, 2.0);
+            Assert.IsTrue(tri.isScalene());
+        }
+
+        [TestMethod]
+        public void isIsoscelesTest() {
+            Triangle tri = new Triangle(1.0, 0.5, 1.0);
+            Assert.IsTrue(tri.isIsosceles());
+            tri = new Triangle(1.0, 0.5, 2.0);
+            Assert.IsFalse(tri.isIsosceles());
+        }
+
+        [TestMethod]
+        public void isEquilateralTest() {
+            Triangle tri = new Triangle(1.0, 1.0, 1.0);
+            Assert.IsTrue(tri.isEquilateral());
+        }
+    }
+}
