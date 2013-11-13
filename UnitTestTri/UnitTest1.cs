ab45ff39c58a6da834d2fae63527e50af01d5827
re222dv <re222dv@student.lnu.se>
2013-11-13 13:23:05 +0100

    More constructor tests

--- UnitTestTri/UnitTest1.cs
+++ UnitTestTri/UnitTest1.cs
@@ -8,14 +8,24 @@ namespace UnitTestTri {
         [TestMethod]
         public void ConstructorDoubleDoubleDoubleTest() {
             Triangle tri = new Triangle(1.0, 0.5, 2.0);
-            Console.WriteLine((double[]) GetFieldValue(tri, "sides"));
             CollectionAssert.AreEqual(new double[] { 1, 0.5, 2 }, (double[]) GetFieldValue(tri, "sides"));
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void ConstructorDoubleDoubleDoubleNegativeValuesTest() {
+            Triangle tri = new Triangle(-1.0, 0.5, 2.0);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void ConstructorDoubleDoubleDoubleZeroValuesTest() {
+            Triangle tri = new Triangle(-1.0, 0, 2.0);
         }
 
         [TestMethod]
         public void ConstructorDoubleArrayTest() {
             Triangle tri = new Triangle(new double[] { 1, 0.5, 2 });
-            Console.WriteLine((double[]) GetFieldValue(tri, "sides"));
             CollectionAssert.AreEqual(new double[] { 1, 0.5, 2 }, (double[]) GetFieldValue(tri, "sides"));
         }
 
@@ -23,6 +33,63 @@ namespace UnitTestTri {
         [ExpectedException(typeof(ArgumentException))]
         public void ConstructorDoubleArrayInvalidLengthTest() {
             Triangle tri = new Triangle(new double[] { 1, 0.5, 2, 3 });
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void ConstructorDoubleArrayNegativeValuesTest() {
+            Triangle tri = new Triangle(new double[] { -1, 0.5, 2 });
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void ConstructorDoubleArrayZeroValuesTest() {
+            Triangle tri = new Triangle(new double[] { -1, 0, 2 });
+        }
+
+        [TestMethod]
+        public void ConstructorPointPointPointTest() {
+            Point a, b, c;
+            a = new Point(0, 0);
+            b = new Point(4, 0);
+            c = new Point(0, 3);
+            Triangle tri = new Triangle(a, c, b);
+            CollectionAssert.AreEqual(new double[] { 3, 4, 5 }, (double[]) GetFieldValue(tri, "sides"));
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void ConstructorPointPointPointSamePointTest() {
+            Point a = new Point(0, 0);
+            Triangle tri = new Triangle(a, a, a);
+        }
+
+        [TestMethod]
+        public void ConstructorPointArrayTest() {
+            Point a, b, c;
+            a = new Point(0, 0);
+            b = new Point(4, 0);
+            c = new Point(0, 3);
+            Triangle tri = new Triangle(new Point[] {a, c, b});
+            CollectionAssert.AreEqual(new double[] { 3, 4, 5 }, (double[]) GetFieldValue(tri, "sides"));
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void ConstructorPointArrayInvalidLengthTest() {
+            Point a, b, c, d;
+            a = new Point(0, 0);
+            b = new Point(4, 0);
+            c = new Point(0, 4);
+            d = new Point(4, 4);
+            Triangle tri = new Triangle(new Point[] { a, b, c, d});
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void ConstructorPointArraySamePointTest() {
+            Point a = new Point(0, 0);
+            Triangle tri = new Triangle(new Point[] { a, a, a });
         }
 
         [TestMethod]
